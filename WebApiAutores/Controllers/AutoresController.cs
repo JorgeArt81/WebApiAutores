6f51bf54594e46d4c66f599f6bf1838b2e5f11b2
Jorge Reyes <aeroestadistica@gmail.com>
2022-11-20 21:14:23 -0500

    Fin Sección 3

---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entities;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Autor>>> Get()
+        {
+            return await context.Autores.Include(autor => autor.Libros).ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")] // api.autores/id
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            var autorExists = await context.Autores.AnyAsync(autor => autor.Id == id);
+
+            if (!autorExists)
+            {
+                return NotFound();
+            }
+
+            if (autor.Id != id)
+            {
+                return BadRequest("El id del autor no coincide con el id de la URL");
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")] // api.autores/id
+        public async Task<ActionResult> Delete(int id)
+        {
+            var autorExists = await context.Autores.AnyAsync(autor => autor.Id == id);
+
+            if (!autorExists)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Autor { Id = id });
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+    }
+}
---
+++ WebApiAutores/Controllers/LibrosController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entities;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/libros")]
+    public class LibrosController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public LibrosController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Libro>> Get(int id)
+        {
+            return await context.Libros.Include(libro => libro.Autor).FirstOrDefaultAsync(libro => libro.Id == id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Libro>> Post(Libro libro)
+        {
+            var autorExists = await context.Autores.AnyAsync(autor => autor.Id == libro.AutorId);
+
+            if (!autorExists)
+            {
+                return BadRequest($"No existe el autor de id: {libro.AutorId}");
+            }
+
+            context.Add(libro);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
